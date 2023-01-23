8b3bad04ee0f374de3f89baced9ff79140ac5f8a
Luca Martinelli <martinelliluca98@gmail.com>
2023-01-23 12:50:46 +0100

    Task from environment

--- utils/tasks.py
+++ utils/tasks.py
@@ -1,5 +1,14 @@
-BIKE_MOTORBIKE = "bikeMotorbike"
-CAT_DOG = "catDog"
-MAN_WOMAN = "manWoman"
+import os
 
-currentTask = BIKE_MOTORBIKE
+TASKS = ["catDog", "manWoman", "bikeMotorbike"]
+
+taskIndex = os.getenv('TASK', 0)
+
+if taskIndex.isdigit() and int(taskIndex) > 0 and int(taskIndex) < len(TASKS):
+  taskIndex = int(taskIndex)
+else:
+  print("[⛔ TASK NON VALIDO] Verrà usato quello predefinito")
+
+  taskIndex = 0
+
+currentTask = TASKS[taskIndex]
